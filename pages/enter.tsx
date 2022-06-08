1e61ef2e582c658a6ce4190940b9d435cc9fcfda
otw7917 <otw7917@gmail.com>
2022-06-08 10:56:04 +0900

    ui:item detail 🎨

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -54,7 +54,7 @@ function Enter() {
             {method === "email" ? (
               <input
                 type='email'
-                className='w-full px-3 py-2 border border-gray-300 rounded-md rounded shadow-md plachoder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 '
+                className='w-full px-3 py-2 border border-gray-300 rounded-md shadow-md plachoder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 '
                 required
               />
             ) : null}
