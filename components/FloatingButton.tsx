a9a6074f5fdf93c5efb90413c4e2abbf80be8f8d
otw7917 <otw7917@gmail.com>
2022-06-11 13:12:59 +0900

    fix: floatingButton children type JSX.Element -> React.ReactNode

--- components/FloatingButton.tsx
+++ components/FloatingButton.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 interface FloatingButtonProps {
-  children?: JSX.Element;
+  children: React.ReactNode;
 }
+
 function FloatingButton({ children }: FloatingButtonProps) {
   return (
     <button className='fixed text-white cursor-progress bottom-20 right-5 p-5 rounded-full transition-colors hover:animate-bounce bg-orange-400 hover:bg-orange-600'>
