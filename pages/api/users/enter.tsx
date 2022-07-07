d9dec053cc114687beb2d5d953d7d29faf141f62
otw7917 <otw7917@gmail.com>
2022-07-07 14:59:47 +0900

    feature login logic with prisma client

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,43 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import client from "../../../libs/client";
+import withHandler from "../../../libs/server/withHandler";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log(req.body);
-  if (req.method !== "POST") {
-    res.status(401).end();
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { phone, email } = req.body;
+  let user;
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      user = await client.user.create({
+        data: {
+          name: "defaultName",
+          email: email,
+        },
+      });
+    }
   }
-  res.status(200).end();
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone,
+      },
+    });
+    if (!user) {
+      user = await client.user.create({
+        data: {
+          name: "defaultName",
+          phone,
+        },
+      });
+    }
+  }
+  console.log(user);
+  return res.status(200).end();
 }
+
+export default withHandler("POST", handler);
